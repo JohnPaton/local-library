57bb01c8751d20133a454f6464d2e348464747fe
JohnPaton <j.paton@catawiki.nl>
2019-11-22 17:04:34 +0100

    Part 6

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+
 from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+
+# index is a function-based view
 def index(request):
     """Home page"""
 
@@ -17,3 +21,32 @@ def index(request):
     num_authors = Author.objects.count()
 
     return render(request, "index.html", context=locals())
+
+
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 10
+    queryset = Book.objects.order_by("title", "author")
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get the context
+        context = super().get_context_data(**kwargs)
+        # Create any data and add it to the context
+        context["my_extra_context_var"] = "This is just some data"
+        return context
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
+
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 10
+    queryset = Author.objects.order_by(
+        "last_name", "first_name", "date_of_birth"
+    )
+
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
