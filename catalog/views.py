69162e6e19f9eaa01427c5e06511765ce7e7ba09
JohnPaton <j.paton@catawiki.nl>
2019-11-22 15:38:19 +0100

    Part 5

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
+def index(request):
+    """Home page"""
+
+    # Generate counts
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+
+    # Available books (status "a")
+    num_instances_available = BookInstance.objects.filter(
+        status__exact="a"
+    ).count()
+
+    num_authors = Author.objects.count()
+
+    return render(request, "index.html", context=locals())
