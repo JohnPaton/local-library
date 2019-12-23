096d7dcda74e8da52711f148025063289d587559
JohnPaton <j.paton@catawiki.nl>
2019-12-23 11:37:37 +0100

    Part 9 challenge yourself

--- catalog/urls.py
+++ catalog/urls.py
@@ -33,4 +33,11 @@ urlpatterns = [
         views.AuthorDelete.as_view(),
         name="author_delete",
     ),
+    path("book/create/", views.BookCreate.as_view(), name="book_create"),
+    path(
+        "book/<int:pk>/update/", views.BookUpdate.as_view(), name="book_update"
+    ),
+    path(
+        "book/<int:pk>/delete/", views.BookDelete.as_view(), name="book_delete"
+    ),
 ]
